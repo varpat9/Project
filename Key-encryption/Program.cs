08a7984b5a9f246e27b5c1ef0bf4ff85e731f686
TSRSE <26186238+TSRSE@users.noreply.github.com>
2023-10-06 12:50:40 +0300

    Рефактор кода Методы не являются вложенными друг в друга, обращаться с объектами стало проще

--- Key-encryption/Program.cs
+++ Key-encryption/Program.cs
@@ -1,109 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-
 
 namespace Key_encryption
 {
     internal class Program
     {
-        static Random rnd = new Random();
-        static char[] charArr;
+        static Random RandomNumber = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите предложение");
-            string sentence = Console.ReadLine();
-            charArr = sentence.ToCharArray();
-            int n;
-            n = Convert.ToInt32(Math.Ceiling(Math.Sqrt(charArr.Length)));
-            MatrixInput(n);
-            Console.ReadKey();
+            // Стоит вынести как меню в будущем
+            while (true)
+            {
+                char[] charArray = InputForm("Введите предложение:").ToCharArray();
+
+                char[,] charMatrix = CreateMatrix(charArray);
+
+                int[] key = CreateKey(charMatrix);
+
+                Console.WriteLine($"Ключ: {string.Join("", key)}");
+                Console.Write("Результат: ");
+                OutputMatrix(charMatrix, key);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            
         }
 
-        //static char[,] InputArr = new char[n, n];
-
-        static void MatrixInput(int number)
+        static string InputForm(string label)
         {
-            char[,] InputArr = new char[number, number];
+            Console.Write($"{label} ");
+            return Console.ReadLine();
+        }
+
+        static int[] CreateKey(char[,] array)
+        {
+            int[] randomKey = new int[array.GetLength(0)];
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            { randomKey[i] = i - 1; }
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int j = RandomNumber.Next(i + 1);
+
+                if (j != i)
+                { randomKey[i] = randomKey[j]; }
+
+                randomKey[j] = i;
+            }
+            return randomKey;
+        }
+
+        static char[,] CreateMatrix(char[] array)
+        {
+            int size = Convert.ToInt32(Math.Ceiling(Math.Sqrt(array.Length)));
+            char[,] charMatrix = new char[size, size];
             //Запись и создание матрицы 
             int k = 0;
-            for (int i = 0; i < number; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < number; j++)
-
+                for (int j = 0; j < size; j++)
                 {
                     try
-                    {
-                        InputArr[i, j] = charArr[k];
-                    }
+                    { charMatrix[i, j] = array[k]; }
                     catch
-                    {
-                        InputArr[i, j] = Convert.ToChar(' ');
-                    }
+                    { charMatrix[i, j] = Convert.ToChar(' '); }
                     k++;
                 }
             }
-            MatrixMix(number, InputArr);
+            return charMatrix;
         }
 
-
-
-        static void MatrixMix(int number, char[,] Array)
-        {
-            //тестовый ключ
-            //int[] RanKey = { 0, 2, 1, 3 };
-
-            //шифрование и получение ключа
-            int[] RanKey = new int[number];
-
-
-            for (int i = 0; i < number; i++)
-            {
-                RanKey[i] = i - 1;
-            }
-
-
-            for (int i = 0; i < number; i++)
-            {
-                int j = rnd.Next(i + 1);
-                if (j != i)
-                {
-                    RanKey[i] = RanKey[j];
-                }
-                RanKey[j] = i;
-            }
-            Console.WriteLine($"Ключ:");
-            for (int i = 0; i < number; i++)
-            {
-
-                Console.Write(RanKey[i]);
-            }
-            Console.WriteLine(' ');
-            Console.WriteLine("Результат:");
-            MatrixOutput(number, Array, RanKey);
-        }
-
-        static void MatrixOutput(int number, char[,] Array, int[] Rankey)
+        static void OutputMatrix(char[,] array, int[] key)
         {
             //конвертация в строку и вывод вместе с ключом
-            char[,] MixedArr = new char[number, number];
-            for (int i = 0; i < number; i++)
+            char[,] mixedArr = new char[array.GetLength(0), array.GetLength(1)];
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < number; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    MixedArr[i, j] = Array[i, Rankey[j]];
-                    Console.Write(MixedArr[i, j]);
+                    mixedArr[i, j] = array[i, key[j]];
+                    Console.Write(mixedArr[i, j]);
                 }
             }
         }
 
+        /*
         static void Deshifr()
         {
 
-        }
+        } */
     }
 }
